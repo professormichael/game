95dca3206379a2f22c49fba5a77eb49a537419e0
professormichael <77131080+professormichael@users.noreply.github.com>
2024-01-06 12:53:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function button(num){
+  let value1 = document.getElementById("input");
+  value1.value += num;
+  
+}
+function calculate() {
+   var y= eval(document.getElementById('input').value);
+document.getElementById('input').value = y;
+}
+ function delete_last() {
+   var x = document.getElementById('input').value;
+
+   // Convert the string to an array, remove the last character, and join back to a string
+   var y = x.split('');
+   y.splice(y.length - 1, 1);
+   y = y.join('');
+
+   document.getElementById('input').value = y;
+ }
+
+
